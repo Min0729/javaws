a26a23a32896ec44934352309ae4d07ee6ee5ca7
Min0729 <keyti0729@naver.com>
2023-07-24 11:53:07 +0900

    first commit

---
+++ javaPrj/src/day4/MyInfo.java
@@ -0,0 +1,20 @@
+package day4;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		String name="김민지";
+		String blood = "AB";
+		int age =  25;
+		String birthday="1999.07.29";
+		String phoneNumber="010-3926-4530";
+		boolean isAdult=true;
+		double height=163.5;
+		
+		System.out.println("이름: "+name + ", 나이: "+ age + ", 생일: "+birthday+", 핸드폰번호: "+phoneNumber+", 혈액형: "+blood);
+		System.out.println(isAdult);
+		System.out.println(height);
+		
+	}
+
+}
---
+++ javaPrj/src/day4/TenEx.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class TenEx {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=10; i>=1; i--) {
+			sum=sum+i;
+		}
+		
+		System.out.println(sum);
+
+		
+		
+	}
+
+}
